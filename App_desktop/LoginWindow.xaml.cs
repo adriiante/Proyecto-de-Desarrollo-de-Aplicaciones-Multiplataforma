9595edf73d75420d1b1d485828df446af673c329
adriiante <165190645+adriiante@users.noreply.github.com>
2024-04-28 13:41:09 +0200

    UPD app desktop
    Creación de una carpeta para agrupar archivos y nuevas ventanas de inicio de sesión y registro de usuario.

---
+++ App_desktop/LoginWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InfoRace
+{
+    /// <summary>
+    /// Lógica de interacción para RegisterWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tbCrearCuenta_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Aquí puedes abrir la nueva ventana o realizar cualquier acción que desees.
+            // Por ejemplo, abrir una nueva ventana:
+            RegisterWindow registerWindow = new RegisterWindow();
+            registerWindow.Show();
+
+            this.Close();
+        }
+    }
+}
---
+++ App_desktop/RegisterWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InfoRace
+{
+    /// <summary>
+    /// Lógica de interacción para RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tbIniciarSesion_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Aquí puedes abrir la nueva ventana o realizar cualquier acción que desees.
+            // Por ejemplo, abrir una nueva ventana:
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+
+            this.Close();
+        }
+    }
+}
